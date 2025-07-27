a5810dd09cf3646ee62671ba8564a93d0202b5c1
RampandiyarCD <rampandiyar.rengaraj@crystaldelta.com>
2025-07-27 23:24:13 +0530

    added loggers

--- controller/accountController.js
+++ controller/accountController.js
@@ -1,3 +1,4 @@
+import { logger } from "../config/logger.js";
 import {
   createAccountService,
   getAccountService,
@@ -12,10 +13,14 @@ export const createAccount = async (req, res) => {
     const accData = await createAccountService(req.body);
 
     if (accData?.error) {
+      logger.error({ error: accData.error });
       return res.status(404).json({ error: accData.error });
     }
+
+    logger.info("Account Created Successfully", { accData });
     return res.status(200).json({ accData });
   } catch (error) {
+    logger.error("Server Error:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -25,10 +30,14 @@ export const getAccount = async (req, res) => {
     const accData = await getAccountService();
 
     if (accData?.error) {
+      logger.error("Error Fetching Accounts", { error: accData.error });
       return res.status(404).json({ error: accData.error });
     }
+
+    logger.info("Accounts Retrieved Successfully", { accData });
     return res.status(200).json({ accData });
   } catch (error) {
+    logger.error("Server Error:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -38,10 +47,14 @@ export const getAccountById = async (req, res) => {
     const accID = await getAccountByIdService(req.params.accId);
 
     if (accID?.error) {
+      logger.error("Error Fetching Account by ID", { error: accID.error });
       return res.status(404).json({ error: accID.error });
     }
+
+    logger.info("Account Retrieved Successfully", { accID });
     return res.status(200).json({ accID });
   } catch (error) {
+    logger.error("Server Error:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -51,10 +64,14 @@ export const updateAccount = async (req, res) => {
     const accID = await updateAccountService(req.params.accId, req.body);
 
     if (accID?.error) {
+      logger.error("Error Updating Account", { error: accID.error });
       return res.status(404).json({ error: accID.error });
     }
+
+    logger.info("Account Updated Successfully", { accID });
     return res.status(200).json({ accID });
   } catch (error) {
+    logger.error("Server Error:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -64,27 +81,39 @@ export const deleteAccount = async (req, res) => {
     const accID = await deleteAccountService(req.params.accId);
 
     if (accID?.error) {
+      logger.error("Error Deleting Account", { error: accID.error });
       return res.status(404).json({ error: accID.error });
     }
+
+    logger.info("Account Deleted Successfully", { accID });
     return res
       .status(200)
       .json({ message: "Account deleted successfully", account: accID });
   } catch (error) {
+    logger.error("Server Error:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 export const tranferAmount = async (req, res) => {
   try {
-    const {acc, acc2} = await transferAccountService(req.params.accId, req.params.accId2, req.body);
+    const { acc, acc2 } = await transferAccountService(
+      req.params.accId,
+      req.params.accId2,
+      req.body
+    );
 
     if (acc?.error || acc2?.error) {
-      return res.status
-        .status(404)
-        .json({ error: acc.error || acc2.error });
+      logger.error("Error Transferring Amount", {
+        error: acc?.error || acc2?.error,
+      });
+      return res.status(404).json({ error: acc?.error || acc2?.error });
     }
 
-
-    return res.status(200).json({ message: "account ", acc,acc2 });
-  } catch (error) {}
+    logger.info("Amount Transferred Successfully", { acc, acc2 });
+    return res.status(200).json({ message: "Amount transferred", acc, acc2 });
+  } catch (error) {
+    logger.error("Server Error:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
--- controller/statementController.js
+++ controller/statementController.js
@@ -1,3 +1,4 @@
+import { logger } from "../config/logger.js";
 import {
   createStatementService,
   getStatementsService,
@@ -9,23 +10,34 @@ import {
 export const createStatement = async (req, res) => {
   try {
     const statement = await createStatementService(req.body);
+
     if (statement?.error) {
+      logger.error("Error Creating Statement", { error: statement.error });
       return res.status(404).json({ error: statement.error });
     }
-    return res.status(201).json({statement});
+
+    logger.info("Statement Created Successfully", { statement });
+    return res.status(201).json({ statement });
   } catch (error) {
+    logger.error("Server Error:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 export const getStatements = async (req, res) => {
   try {
+    const { accountId } = req.params; // assuming accountId is passed via params
     const statement = await getStatementsService(accountId);
+
     if (statement?.error) {
+      logger.error("Error Fetching Statements", { error: statement.error });
       return res.status(404).json({ error: statement.error });
     }
-    return res.status(200).json({statement});
+
+    logger.info("Statements Retrieved Successfully", { statement });
+    return res.status(200).json({ statement });
   } catch (error) {
+    logger.error("Server Error:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -33,11 +45,18 @@ export const getStatements = async (req, res) => {
 export const getStatementById = async (req, res) => {
   try {
     const statement = await getStatementByIdService(req.params.id);
+
     if (statement?.error) {
+      logger.error("Error Fetching Statement by ID", {
+        error: statement.error,
+      });
       return res.status(404).json({ error: statement.error });
     }
-    return res.status(200).json({statement});
+
+    logger.info("Statement Retrieved Successfully", { statement });
+    return res.status(200).json({ statement });
   } catch (error) {
+    logger.error("Server Error:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -45,11 +64,16 @@ export const getStatementById = async (req, res) => {
 export const updateStatement = async (req, res) => {
   try {
     const statement = await updateStatementService(req.params.id, req.body);
+
     if (statement?.error) {
+      logger.error("Error Updating Statement", { error: statement.error });
       return res.status(404).json({ error: statement.error });
     }
-    return res.status(200).json({statement});
+
+    logger.info("Statement Updated Successfully", { statement });
+    return res.status(200).json({ statement });
   } catch (error) {
+    logger.error("Server Error:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -57,11 +81,16 @@ export const updateStatement = async (req, res) => {
 export const deleteStatement = async (req, res) => {
   try {
     const statement = await deleteStatementService(req.params.id);
+
     if (statement?.error) {
+      logger.error("Error Deleting Statement", { error: statement.error });
       return res.status(404).json({ error: statement.error });
     }
-    return res.status(200).json({statement});
+
+    logger.info("Statement Deleted Successfully", { statement });
+    return res.status(200).json({ statement });
   } catch (error) {
+    logger.error("Server Error:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
